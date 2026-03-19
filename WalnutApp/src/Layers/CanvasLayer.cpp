9c4468fcaf6f2819835ce763a402bf6e256f314d
andrew georgiou <andrew.georgiou03@gmail.com>
2026-03-19 15:29:48 +0000

    Customizable canvas dimensions, primary and secondary colours, thicker brush. Ready to move to compute shader implementation

--- WalnutApp/src/Layers/CanvasLayer.cpp
+++ WalnutApp/src/Layers/CanvasLayer.cpp
@@ -3,13 +3,16 @@
 #include <glm/glm.hpp>
 #include <iostream>
 
+CanvasLayer::CanvasLayer(uint32_t canvasWidth, uint32_t canvasHeight)
+	: m_CanvasWidth(canvasWidth), m_CanvasHeight(canvasHeight) {}
+
 void CanvasLayer::OnUIRender()
 {
 	ImGui::Begin("Canvas");
 
 	if (m_CanvasImage)
 	{
-		ImGui::Image(m_CanvasImage->GetDescriptorSet(), { 512, 512 });
+		ImGui::Image(m_CanvasImage->GetDescriptorSet(), { static_cast<float>(m_CanvasWidth), static_cast<float>(m_CanvasHeight) });
 	}
 
 	ImVec2 minImageBounds = ImGui::GetItemRectMin();
@@ -19,49 +22,64 @@ void CanvasLayer::OnUIRender()
 	float x = mousePos.x - minImageBounds.x;
 	float y = mousePos.y - minImageBounds.y;
 
-	std::cout << "x: " << x << ", y: " << y << "\n";
-
 	// TODO: dispatch compute shader
 	if (ImGui::IsMouseDown(0))
 	{
-		if (x < 0 || x > m_CanvasImage->GetWidth() - 1 || y < 0 || y > m_CanvasImage->GetHeight() - 1)
-		{
-			std::cout << "OUT OF BOUNDS CLICK\n";
-		}
-		else
-		{
-			std::cout << "IN BOUNDS CLICK\n";
-
-			uint32_t index = x + y * 512;
-			m_CanvasData[index] = 255 << 24 | 0 << 16 | 0 << 8 | 255;
-
-			m_CanvasImage->SetData(m_CanvasData.get());
-		}
+		uint32_t colour = 255 << 24 | 0 << 16 | 0 << 8 | 255;
+		DrawWithRadius(x, y, 5, colour);
+	}
+	else if (ImGui::IsMouseDown(1))
+	{
+		uint32_t colour = 255 << 24 | 255 << 16 | 0 << 8 | 0;
+		DrawWithRadius(x, y, 5, colour);
 	}
 
 	ImGui::End();
+}
+
+void CanvasLayer::DrawWithRadius(int32_t centreX, int32_t centreY, int radius, uint32_t colour)
+{
+	int32_t xMin = centreX - radius;
+	int32_t xMax = centreX + radius;
+
+	int32_t yMin = centreY - radius;
+	int32_t yMax = centreY + radius;
+
+	for (int32_t y = yMin; y < yMax; ++y)
+	{
+		for (int32_t x = xMin; x < xMax; ++x)
+		{
+			if (IsInBounds(x, y))
+			{
+				uint32_t index = static_cast<uint32_t>(x + y * m_CanvasWidth);
+				m_CanvasData[index] = colour;
+
+				m_CanvasImage->SetData(m_CanvasData.get());
+			}
+		}
+	}
 }
 
 void CanvasLayer::OnAttach()
 {
 	if (!m_CanvasImage)
 	{
-		m_CanvasImage = std::make_shared<Walnut::Image>(512, 512, Walnut::ImageFormat::RGBA);
+		m_CanvasImage = std::make_shared<Walnut::Image>(m_CanvasWidth, m_CanvasHeight, Walnut::ImageFormat::RGBA);
 	}
 
 	if (!m_CanvasData)
 	{
-		m_CanvasData = std::make_unique<uint32_t[]>(uint32_t(512 * 512));
+		m_CanvasData = std::make_unique<uint32_t[]>(uint32_t(m_CanvasWidth * m_CanvasHeight));
 	}
 
-	for (uint32_t y = 0; y < 512; ++y)
+	for (uint32_t y = 0; y < m_CanvasHeight; ++y)
 	{
-		for (uint32_t x = 0; x < 512; ++x)
+		for (uint32_t x = 0; x < m_CanvasWidth; ++x)
 		{
-			uint32_t index = x + y * 512;
+			uint32_t index = x + y * m_CanvasWidth;
 
-			uint8_t r = glm::floor(static_cast<double>(x) / 512 * 255);
-			uint8_t g = glm::floor(static_cast<double>(y) / 512 * 255);
+			uint8_t r = glm::floor(static_cast<double>(x) / m_CanvasWidth * 255);
+			uint8_t g = glm::floor(static_cast<double>(y) / m_CanvasHeight * 255);
 			uint8_t b = 0;
 			uint8_t a = 255;
 
@@ -71,4 +89,9 @@ void CanvasLayer::OnAttach()
 	}
 
 	m_CanvasImage->SetData(m_CanvasData.get());
+}
+
+bool CanvasLayer::IsInBounds(float x, float y)
+{
+	return x >= 0 && x < m_CanvasImage->GetWidth() && y >= 0 && y < m_CanvasImage->GetHeight();
 }
--- WalnutApp/src/Layers/CanvasLayer.h
+++ WalnutApp/src/Layers/CanvasLayer.h
@@ -5,10 +5,18 @@
 class CanvasLayer final : public Walnut::Layer
 {
 public:
+	CanvasLayer() = delete;
+	CanvasLayer(uint32_t canvasWidth, uint32_t canvasHeight);
+
 	virtual void OnAttach() override;
 	virtual void OnUIRender() override;
 private:
+	bool IsInBounds(float x, float y);
+	void DrawWithRadius(int32_t centreX, int32_t centreY, int radius, uint32_t colour);
+private:
 	std::shared_ptr<Walnut::Image> m_CanvasImage;
 	std::unique_ptr<uint32_t[]> m_CanvasData;
-	//uint32_t* m_CanvasData;
+
+	uint32_t m_CanvasHeight;
+	uint32_t m_CanvasWidth;
 };
