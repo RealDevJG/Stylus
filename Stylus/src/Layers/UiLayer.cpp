8b8c427702bd0b8a420a0e376eb2979d3c4cd14c
andrew georgiou <andrew.georgiou03@gmail.com>
2026-04-29 17:10:06 +0100

    Default window layout has been added. Layout can be reverted to the default whenever the user wants using the View button menu in the title bar

--- Stylus/src/Layers/UiLayer.cpp
+++ Stylus/src/Layers/UiLayer.cpp
@@ -8,11 +8,35 @@
 #include "../Systems/ToolRegistry.h"
 
 #include <imgui.h>
+#include <imgui_internal.h>
+
+#include <fstream>
 
 namespace Stylus {
 
+	void UiLayer::OnAttach()
+	{
+		std::fstream fstream("./imgui.ini");
+
+		if (!fstream.good())
+		{
+			m_ShouldSetDefaultLayout = true;
+		}
+	}
+
 	void UiLayer::OnUIRender()
 	{
+		if (m_InitialLoad)
+		{
+			SetWindowProperties();
+			m_InitialLoad = false;
+		}
+
+		if (m_ShouldSetDefaultLayout)
+		{
+			DefaultLayout();
+		}
+
 		ImGui::Begin("Tool Bar");
 
 		const auto& tools = m_ToolRegistry->GetTools();
@@ -33,6 +57,51 @@ namespace Stylus {
 		}
 
 		ImGui::End();
+	}
+
+	void UiLayer::SetDefaultLayout()
+	{
+		m_ShouldSetDefaultLayout = true;
+	}
+
+	void UiLayer::SetWindowProperties()
+	{
+		ImGui::Begin("Canvas"); ImGui::End();
+		ImGui::Begin("Tool Bar"); ImGui::End();
+		ImGui::Begin("Tool Options"); ImGui::End();
+
+		auto* canvas = ImGui::FindWindowByID(ImHashStr("Canvas"));
+		auto* toolOptions = ImGui::FindWindowByID(ImHashStr("Tool Options"));
+		auto* toolBar = ImGui::FindWindowByID(ImHashStr("Tool Bar"));
+
+		canvas->WindowClass.DockNodeFlagsOverrideSet = ImGuiDockNodeFlags_NoTabBar;
+		toolBar->WindowClass.DockNodeFlagsOverrideSet = ImGuiDockNodeFlags_NoTabBar;
+		toolOptions->WindowClass.DockNodeFlagsOverrideSet = ImGuiDockNodeFlags_NoTabBar;
+	}
+
+	void UiLayer::DefaultLayout()
+    {
+        ImGuiID dockspaceId = ImGui::GetID("MyDockspace");
+
+		if (ImGui::FindWindowByID(ImHashStr("Canvas")) != nullptr && ImGui::FindWindowByID(ImHashStr("Tool Bar")) != nullptr)
+        {
+			ImGui::DockBuilderRemoveNode(dockspaceId);
+			ImGui::DockBuilderAddNode(dockspaceId, ImGuiDockNodeFlags_DockSpace);
+			ImGui::DockBuilderSetNodeSize(dockspaceId, ImGui::GetMainViewport()->Size);
+
+			SetWindowProperties();
+
+			ImGuiID dockCanvasId = dockspaceId; 
+			ImGuiID dockOptionsId = ImGui::DockBuilderSplitNode(dockCanvasId, ImGuiDir_Up, 0.07f, nullptr, &dockCanvasId);
+			ImGuiID dockToolsId = ImGui::DockBuilderSplitNode(dockCanvasId, ImGuiDir_Left, 0.11f, nullptr, &dockCanvasId);
+
+			ImGui::DockBuilderDockWindow("Tool Options", dockOptionsId);
+			ImGui::DockBuilderDockWindow("Tool Bar", dockToolsId);
+			ImGui::DockBuilderDockWindow("Canvas", dockCanvasId);
+
+			ImGui::DockBuilderFinish(dockspaceId);
+			m_ShouldSetDefaultLayout = false;
+		}
 	}
 
 }
--- Stylus/src/Layers/UiLayer.h
+++ Stylus/src/Layers/UiLayer.h
@@ -16,8 +16,17 @@ namespace Stylus {
 		UiLayer(std::shared_ptr<ToolManager> toolManager, std::shared_ptr<ToolRegistry> toolRegistry)
 			: m_ToolManager(toolManager), m_ToolRegistry(toolRegistry), m_UiDrawer(toolManager) {}
 
+		virtual void OnAttach() override;
 		virtual void OnUIRender() override;
+
+		void SetDefaultLayout();
+		void SetWindowProperties();
 	private:
+		void DefaultLayout();
+	private:
+		bool m_ShouldSetDefaultLayout = false;
+		bool m_InitialLoad = true;
+
 		UIDrawer m_UiDrawer;
 
 		std::shared_ptr<ToolManager> m_ToolManager;
